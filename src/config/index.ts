815b8f8a369e2a625016d88b7cac60884392b1c3
OriLight <61224409+orilights@users.noreply.github.com>
2023-04-06 00:29:34 +0800

    chore: 更换原图加载地址 E5 订阅翻车惹 /(ㄒoㄒ)/~~

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export const githubLink = 'https://github.com/orilights/PixivCollection'
-export const originalImageHost = 'https://dl.orilight.top/d/pixiv/'
+export const originalImageHost = 'https://pixiv.orilight.top/image/original/'
 export const pixivArtworkLink = 'https://www.pixiv.net/artworks/'
 export const pixivUserLink = 'https://www.pixiv.net/users/'
 
