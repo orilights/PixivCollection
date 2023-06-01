baf877eccd363a6d16a26a62d6abfc8b9b58b9be
OriLight <amatlsmisr@gmail.com>
2023-06-01 16:35:16 +0800

    revert: "fix: 修复可能频繁触发的图片重排序问题" 此 commit 会导致页面滚动卡顿 This reverts commit 608c41633ee0c7443871fe252c617663d7675a98.

--- src/components/MasonryView/index.vue
+++ src/components/MasonryView/index.vue
@@ -1,40 +1,31 @@
 <template>
   <div
-    v-show="imagesFiltered.length"
-    ref="scrollerView" class="overflow-y-scroll"
+    ref="container" class="mx-auto relative"
     :class="{
-      'h-[calc(100vh-60px)] mt-[60px]': showNav,
-      'h-screen': !showNav,
+      'lg:w-[960px]': !masonryConfig.containerFullWidth,
+    }"
+    :style="{
+      height: `${Math.max(...colsTop) + masonryConfig.gap}px`,
+      padding: `${masonryConfig.gap}px`,
     }"
   >
-    <div
-      ref="container" class="mx-auto relative"
-      :class="{
-        'lg:w-[960px]': !masonryConfig.containerFullWidth,
+    <MasonryViewItem
+      v-for="item in imagesRenderList" :key="`${item.image.id}_${item.image.part}`" :index="item.idx"
+      :image="item.image"
+      :show-no="masonryConfig.showImageNo"
+      :tag-include-bookmark="filterConfig.tag.includeBookmark"
+      :tag-translation="masonryConfig.showTagTranslation"
+      :info-at-bottom="masonryConfig.infoAtBottom"
+      :shadow="masonryConfig.gap > 2"
+      :load-image="imagesShow.includes(item.idx)" :style="{
+        width: `calc((100% - ${masonryConfig.gap * (col + 1)}px) / ${col})`,
+        height: `${getImageHeight(item.image.size) + (masonryConfig.infoAtBottom ? 120 : 0)}px`,
+        transform: `translate(${item.left}px, ${item.top}px)`,
       }"
-      :style="{
-        height: `${Math.max(...colsTop) + masonryConfig.gap}px`,
-        padding: `${masonryConfig.gap}px`,
-      }"
-    >
-      <MasonryViewItem
-        v-for="item in imagesRenderList" :key="`${item.image.id}_${item.image.part}`" :index="item.idx"
-        :image="item.image"
-        :show-no="masonryConfig.showImageNo"
-        :tag-include-bookmark="filterConfig.tag.includeBookmark"
-        :tag-translation="masonryConfig.showTagTranslation"
-        :info-at-bottom="masonryConfig.infoAtBottom"
-        :shadow="masonryConfig.gap > 2"
-        :load-image="imagesShow.includes(item.idx)" :style="{
-          width: `calc((100% - ${masonryConfig.gap * (col + 1)}px) / ${col})`,
-          height: `${getImageHeight(item.image.size) + (masonryConfig.infoAtBottom ? 120 : 0)}px`,
-          transform: `translate(${item.left}px, ${item.top}px)`,
-        }"
-        @open-image="openImageViewer" @open-pixiv="openPixiv" @open-pixiv-user="openPixivUser"
-        @filter-author="filterAuthor"
-        @destory="itemDestroy"
-      />
-    </div>
+      @open-image="openImageViewer" @open-pixiv="openPixiv" @open-pixiv-user="openPixivUser"
+      @filter-author="filterAuthor"
+      @destory="itemDestroy"
+    />
   </div>
 </template>
 
@@ -44,8 +35,7 @@ import { useStore } from '@/store'
 import { pixivArtworkLink, pixivUserLink } from '@/config'
 
 const store = useStore()
-const { filterConfig, imagesFiltered, masonryConfig, showNav } = toRefs(store)
-const scrollerView = ref()
+const { filterConfig, imagesFiltered, masonryConfig } = toRefs(store)
 const container = ref()
 const { width: containerWidthO } = useElementSize(container)
 const { top: containerTopO } = useElementBounding(container)
@@ -97,7 +87,6 @@ const imagesRenderList = computed(() => {
   return imagesPlaced.value
 })
 const imagesShow = ref<number[]>([])
-let oldY = 0
 
 const lazyloadImage = useDebounceFn(() => {
   nextTick(() => {
@@ -113,18 +102,7 @@ const lazyloadImage = useDebounceFn(() => {
 
 onMounted(() => {
   lazyloadImage()
-  scrollerView.value.addEventListener('scroll', lazyloadImage, { passive: true })
-  scrollerView.value.addEventListener('scroll', () => {
-    const newY = scrollerView.value.scrollTop
-    if (newY > oldY && newY > 200)
-      showNav.value = false
-    else if (newY < oldY)
-      showNav.value = true
-    oldY = newY
-  }, { passive: true })
-  setInterval(() => {
-    lazyloadImage()
-  }, 1000)
+  window.addEventListener('scroll', lazyloadImage, { passive: true })
 })
 
 watch(imagesFiltered, () => {
--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,59 +1,61 @@
 <template>
-  <div
-    class="fixed top-0 left-0 w-full h-[60px] z-10 bg-white dark:bg-[#202020] transition-[transform,background] shadow-md"
-    :class="{
-      'translate-y-[-70px]': (!showNav || imageViewerShow) && !showSidebar,
-    }"
-  >
+  <div class="h-[60px]">
     <div
-      class="absolute top-0 left-0 flex"
+      class="fixed top-0 left-0 w-full h-[60px] z-10 bg-white dark:bg-[#202020] transition-[transform,background] shadow-md"
       :class="{
-        'w-full': filterConfig.search.enable,
+        'translate-y-[-70px]': (!showNav || imageViewerShow) && !showSidebar,
       }"
     >
-      <button class="w-[60px] h-[60px] hover:bg-gray-400/20" @click="showSidebar = !showSidebar">
-        <IconMenu class="w-7 h-7 mx-auto" />
-      </button>
-      <button class="w-[60px] h-[60px] hover:bg-gray-400/20 hidden sm:block" @click="store.toggleSearch">
+      <div
+        class="absolute top-0 left-0 flex"
+        :class="{
+          'w-full': filterConfig.search.enable,
+        }"
+      >
+        <button class="w-[60px] h-[60px] hover:bg-gray-400/20" @click="showSidebar = !showSidebar">
+          <IconMenu class="w-7 h-7 mx-auto" />
+        </button>
+        <button class="w-[60px] h-[60px] hover:bg-gray-400/20 hidden sm:block" @click="store.toggleSearch">
+          <IconSearch v-if="!filterConfig.search.enable" class="w-6 h-6 mx-auto" />
+          <IconClose v-else class="w-6 h-6 mx-auto" />
+        </button>
+        <div v-show="filterConfig.search.enable" class="flex-1 mr-[60px] sm:mr-0">
+          <input
+            class="w-full h-[60px] outline-none px-4 box-border border-x border-gray-400/50 bg-transparent" type="text" placeholder="图片id/图片标题/作者id/作者昵称"
+            :value="filterConfig.search.value"
+            @input="handleSearchInput"
+          >
+        </div>
+      </div>
+      <button class="w-[60px] h-[60px] hover:bg-gray-400/20 absolute right-0 block sm:hidden" @click="store.toggleSearch">
         <IconSearch v-if="!filterConfig.search.enable" class="w-6 h-6 mx-auto" />
         <IconClose v-else class="w-6 h-6 mx-auto" />
       </button>
-      <div v-show="filterConfig.search.enable" class="flex-1 mr-[60px] sm:mr-0">
-        <input
-          class="w-full h-[60px] outline-none px-4 box-border border-x border-gray-400/50 bg-transparent" type="text" placeholder="图片id/图片标题/作者id/作者昵称"
-          :value="filterConfig.search.value"
-          @input="handleSearchInput"
-        >
+      <div v-show="!filterConfig.search.enable" class="text-lg h-[60px] leading-[60px] select-none text-center mx-[60px]" @dblclick="navToTop">
+        OriLightの<span class="text-[#0398fa] px-1">Pixiv</span>收藏夹
       </div>
-    </div>
-    <button class="w-[60px] h-[60px] hover:bg-gray-400/20 absolute right-0 block sm:hidden" @click="store.toggleSearch">
-      <IconSearch v-if="!filterConfig.search.enable" class="w-6 h-6 mx-auto" />
-      <IconClose v-else class="w-6 h-6 mx-auto" />
-    </button>
-    <div v-show="!filterConfig.search.enable" class="text-lg h-[60px] leading-[60px] select-none text-center mx-[60px]" @dblclick="navToTop">
-      OriLightの<span class="text-[#0398fa] px-1">Pixiv</span>收藏夹
-    </div>
-    <div class="absolute top-0 right-0 hidden lg:flex">
-      <button
-        class="w-[60px] h-[60px] hover:bg-gray-400/20"
-        @click="openGithub"
-      >
-        <IconGithub class="w-6 h-6 mx-auto" />
-      </button>
-      <button
-        class="w-[60px] h-[60px] hover:bg-gray-400/20"
-        @click="store.toggleDarkMode"
-      >
-        <IconSun v-if="!darkMode" class="w-6 h-6 mx-auto" />
-        <IconMoon v-else class="w-5 h-5 mx-auto" />
-      </button>
-      <button
-        class="w-[60px] h-[60px] hover:bg-gray-400/20"
-        @click="store.toggleFullscreen"
-      >
-        <IconShrink v-if="isFullscreen" class="w-5 h-5 mx-auto" />
-        <IconExpand v-else class="w-5 h-5 mx-auto" />
-      </button>
+      <div class="absolute top-0 right-0 hidden lg:flex">
+        <button
+          class="w-[60px] h-[60px] hover:bg-gray-400/20"
+          @click="openGithub"
+        >
+          <IconGithub class="w-6 h-6 mx-auto" />
+        </button>
+        <button
+          class="w-[60px] h-[60px] hover:bg-gray-400/20"
+          @click="store.toggleDarkMode"
+        >
+          <IconSun v-if="!darkMode" class="w-6 h-6 mx-auto" />
+          <IconMoon v-else class="w-5 h-5 mx-auto" />
+        </button>
+        <button
+          class="w-[60px] h-[60px] hover:bg-gray-400/20"
+          @click="store.toggleFullscreen"
+        >
+          <IconShrink v-if="isFullscreen" class="w-5 h-5 mx-auto" />
+          <IconExpand v-else class="w-5 h-5 mx-auto" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -70,11 +72,21 @@ const updateSearchStr = useDebounceFn((value) => {
   store.updateSeatchValue(value)
 }, 300)
 
+let oldY = 0
+
 onMounted(() => {
   if (store.urlParams.search) {
     store.updateSeatchValue(store.urlParams.search as string)
     store.toggleSearch()
   }
+  window.addEventListener('scroll', () => {
+    const newY = document.documentElement.scrollTop
+    if (newY > oldY && newY > 200)
+      showNav.value = false
+    else if (newY < oldY)
+      showNav.value = true
+    oldY = newY
+  })
 })
 
 function navToTop() {
